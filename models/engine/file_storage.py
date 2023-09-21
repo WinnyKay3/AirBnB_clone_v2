6730316db01ff13f032007f0991483eb3398f09e
gashug <geoffrey.gachuiri98@gmail.com>
2023-09-21 09:33:56 -0700

    Fixed console program

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -10,7 +10,7 @@ class FileStorage:
 
     def all(self,cls=None):
         """Returns a dictionary of models currently in storage"""
-        if cls is None:
+        if cls == None:
 	    return self.__objects
 	
 	filtered_objects = {}
