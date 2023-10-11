d49c43cde0aaccaaffbb2ddf8c4c068c438ae16f
WinnyKay3 <winffredkalondu@gmail.com>
2023-10-11 20:03:07 +0000

    Update 1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -5,10 +5,11 @@ from fabric.api import *
 from datetime import datetime
 from os import path
 
+
 def do_pack():
-    ''' generate a .tgz archive from the contents of web_static 
-       folder into a .tgz archive.
-       Archive path if successful none upon failure'''
+    """generate a .tgz archive from the contents of web_static
+    folder into a .tgz archive.
+    Archive path if successful none upon failure"""
     try:
         now = datetime.now()
         timestamp = now.strftime("%Y%m%d%H%M%S")
@@ -16,7 +17,11 @@ def do_pack():
         print("Packing web_static to {}".format(archive_name))
         local("mkdir -p versions")
         local("tar -czvf {} web_static".format(archive_name))
-        print("web_static packed: {} -> {}Bytes".format(archive_name, path.getsize(archive_name)))
+        print(
+            "web_static packed: {} -> {}Bytes".format(
+                archive_name, path.getsize(archive_name)
+            )
+        )
         return archive_name
     except Exception:
         return None
