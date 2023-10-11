786ff3208bf1dacd2454a976ebe1cf85106dee2e
WinnyKay3 <winffredkalondu@gmail.com>
2023-10-11 20:32:11 +0000

    2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -21,7 +21,6 @@ def do_deploy(archive_path):
         return False
     file = archive_path.split("/")[-1]
     name = file.split(".")[0]
-    run("rm -rf /data/web_static/releases/{}/".format(name))
     if put(archive_path, "/tmp/{}".format(file)).failed is True:
         return False
     if run("mkdir -p /data/web_static/releases/{}/".
